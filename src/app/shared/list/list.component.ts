b939328cbd0528efba143b8dbe9ee1764d8dc1a2
Felipe Silva <felipe.allware@gmail.com>
2023-07-25 16:25:28 -0300

    Finalizando o projeto incrementando animações, tratativas de erros e página de detalhes do pokémon

--- src/app/shared/list/list.component.ts
+++ src/app/shared/list/list.component.ts
@@ -10,18 +10,25 @@ export class ListComponent implements OnInit {
   private allPokemonList: any;
   public pokemonList: any;
 
+  public apiError: boolean = false;
+
   constructor(private pokeApiService: PokeApiService) { }
 
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemons.subscribe((res) => {
-      this.allPokemonList = res.results;
-      this.pokemonList = this.allPokemonList;
-    });
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      (res) => {
+        this.allPokemonList = res.results;
+        this.pokemonList = this.allPokemonList;
+      },
+      (err) => {
+        this.apiError = true;
+      }
+    );
   }
 
   public search(q: string): void {
     const filter = this.allPokemonList.filter((res: any) => {
-      return !res.name.indexOf(q.toLowerCase())
+      return res.name.includes(q.toLowerCase())
     })
 
     this.pokemonList = filter;
